b96c82fd1ba81fdf67ed2dd0cbee9ff275e9dc00
Biraj Pokharel <buraj.sainik@gmail.com>
2021-07-26 15:04:47 +0545

    removed logo centered

--- client/src/layouts/Minimal/components/Topbar.jsx
+++ client/src/layouts/Minimal/components/Topbar.jsx
@@ -3,15 +3,7 @@ import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Paper from '@material-ui/core/Paper';
-import InputBase from '@material-ui/core/InputBase';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import SearchIcon from '@material-ui/icons/Search';
-import CloseIcon from '@material-ui/icons/Close';
-
 import Logo from '../../../assets/img/logo.svg';
-import { Hidden } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -22,24 +14,13 @@ const useStyles = makeStyles((theme) => ({
     [theme.breakpoints.down('sm')]: {
       width: '60%'
     }
-  },
-  toolbar: {
-    [theme.breakpoints.down('sm')]: {
-      display: 'flex',
-      justifyContent: 'center'
-    }
   }
 }));
 
 const Topbar = (props) => {
   const classes = useStyles();
   return (
-    <AppBar
-      className={classes.root}
-      color="transparent"
-      position="static"
-      elevation={0}
-    >
+    <AppBar className={classes.root} position="static" elevation={0}>
       <Toolbar className={classes.toolbar}>
         <>
           <img className={classes.logo} alt="Logo" src={Logo} />
