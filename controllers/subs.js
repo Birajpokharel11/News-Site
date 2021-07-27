4538aabfa2382933fe03110d3ed560b364d8b4a2
Nikhil Shrestha <nikhil.shrestha1995@gmail.com>
2021-07-27 18:25:12 +0545

    fixed formatting

--- controllers/subs.js
+++ controllers/subs.js
@@ -1,18 +1,25 @@
-const Post = require('../models/subscribe');
 const asyncHandler = require('../middleware/async');
 const ErrorResponse = require('../utils/errorResponse');
 
-exports.getsubs = asyncHandler(async(req, res, next) => {
-  const posts = await Post.find()
+const Post = require('../models/subscribe');
+
+// @desc    Get Subscriber
+// @route   GET /api/v1/subscribe
+// @access  Public
+exports.getsubs = asyncHandler(async (req, res, next) => {
+  const posts = await Post.find();
   if (!posts) {
     return next(
-        new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
     );
   }
   res.status(200).json({ success: true, data: posts });
 });
 
-exports.createSubs = asyncHandler(async(req, res) => {
+// @desc    Create Subscriber
+// @route   POST /api/v1/subscribe
+// @access  Public
+exports.createSubs = asyncHandler(async (req, res) => {
   const post = await Post.create(req.body);
   res.status(200).json({
     success: true,
--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.json());
 
 // dev logging middleware
 if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
+  app.use(morgan('dev'));
 }
 
 // set security headers
@@ -38,12 +38,12 @@ app.use(hpp());
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
-    // set static folder
-    app.use(express.static('client/build'));
+  // set static folder
+  app.use(express.static('client/build'));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 // mount routers
