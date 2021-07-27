1460d626eef4ba6aa6aebec365dbd1604dbc4ef0
Nikhil Shrestha <nikhil.shrestha1995@gmail.com>
2021-07-27 18:54:04 +0545

    setup for dev

--- client/src/store/news/news.sagas.js
+++ client/src/store/news/news.sagas.js
@@ -17,14 +17,11 @@ export function* fetchNewsAsync({ payload: { country, companies, themes } }) {
   }
 
   try {
-    const { data } = yield axios.post(
-      'https://api.datacenterinvest.asia/api/v1/news/search',
-      {
-        country,
-        companies,
-        themes
-      }
-    );
+    const { data } = yield axios.post('/api/v1/news/search', {
+      country,
+      companies,
+      themes
+    });
     yield put(actions.fetchNewsSuccess(data.items));
   } catch (err) {
     yield put(actions.fetchNewsFail(err));
@@ -36,12 +33,9 @@ export function* fetchOgTag(item) {
 
   if (!newsState.news[item.guid.text].media) {
     try {
-      const { data } = yield axios.post(
-        'https://api.datacenterinvest.asia/api/v1/news/scrape',
-        {
-          url: item.link
-        }
-      );
+      const { data } = yield axios.post('/api/v1/news/scrape', {
+        url: item.link
+      });
       console.log(data);
 
       yield put(actions.getOGSuccess(item.guid.text, data));
@@ -54,13 +48,10 @@ export function* fetchOgTag(item) {
 
 export function* newsSubscribeAsync({ payload: { email, name } }) {
   try {
-    const { data } = yield axios.post(
-      'https://api.datacenterinvest.asia/api/v1/news/subscribe',
-      {
-        email,
-        name
-      }
-    );
+    const { data } = yield axios.post('/api/v1/news/subscribe', {
+      email,
+      name
+    });
     console.log(data);
 
     yield put(actions.newsSubscribeSuccess(data));
