2c769ad15ce45a83513db7c1e713d16640284b98
Nikhil Shrestha <nikhil.shrestha1995@gmail.com>
2021-07-27 18:13:41 +0545

    fixed setup

--- controllers/subs.js
+++ controllers/subs.js
@@ -1,20 +1,21 @@
 const Post = require('../models/subscribe');
+const asyncHandler = require('../middleware/async');
+const ErrorResponse = require('../utils/errorResponse');
 
-exports.getsubs = (req, res) => {
-  const posts = Post.find()
-    .then((posts) => {
-      res.json({ posts: posts });
-    })
-    .catch((err) => console.log(err));
-};
-exports.createSubs = (req, res) => {
-  const post = new Post(req.body);
-  post.save((err, result) => {
-    if (err) {
-      return res.status(400).json({ error: err });
-    }
-    res.status(200).json({
-      result
-    });
+exports.getsubs = asyncHandler(async(req, res, next) => {
+  const posts = await Post.find()
+  if (!posts) {
+    return next(
+        new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
+  }
+  res.status(200).json({ success: true, data: posts });
+});
+
+exports.createSubs = asyncHandler(async(req, res) => {
+  const post = await Post.create(req.body);
+  res.status(200).json({
+    success: true,
+    data: post
   });
-};
+});
