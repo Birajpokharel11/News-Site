37c58583ed15b1d1039389f087f67a60c8494906
Biraj Pokharel <buraj.sainik@gmail.com>
2021-08-03 01:26:58 +0545

    login and register with jwt token storage

---
+++ client/src/pages/Home/Auth/Login.jsx
@@ -0,0 +1,154 @@
+import React, { useState } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import axios from 'axios';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  }
+}));
+
+export default function SignUp() {
+  const classes = useStyles();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(false);
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+    console.log(email);
+  };
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+    console.log(password);
+  };
+  const isValid = () => {
+    // email@domain.com
+    if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+      setError({
+        error: 'A valid Email is required '
+      });
+      return false;
+    }
+    if (password.length === 0) {
+      setError({ error: 'Password is required' });
+      return false;
+    }
+    return true;
+  };
+  const authenticate = (data) => {
+    console.log('jwt', data.data.token);
+    const jwt = data.data.token;
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('jwt', JSON.stringify(jwt));
+    }
+  };
+  const clickSubmit = (e) => {
+    e.preventDefault();
+    if (isValid()) {
+      axios
+        .post('api/v1/signin', {
+          email,
+          password
+        })
+        .then((data) => authenticate(data));
+      setEmail('');
+      setPassword('');
+    }
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      {console.log('error', error)}
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <form className={classes.form} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                onChange={(e) => handleEmail(e)}
+                value={email}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                onChange={(e) => handlePassword(e)}
+                value={password}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={<Checkbox value="allowExtraEmails" color="primary" />}
+                label="I want to receive inspiration, marketing promotions and updates via email."
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            onClick={clickSubmit}
+            className={classes.submit}
+          >
+            Sign Up
+          </Button>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Link href="#" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
+  );
+}
--- client/src/routes/Routes.js
+++ client/src/routes/Routes.js
@@ -5,7 +5,7 @@ import { RouteWithLayout } from './components';
 import { Minimal as MinimalLayout } from '../layouts';
 
 import { Home as HomePage, NotFound as NotFoundView } from '../pages';
-
+import { Register, Login } from 'src/pages/Home/Auth';
 const Routes = () => {
   return (
     <Switch>
@@ -14,6 +14,18 @@ const Routes = () => {
         exact
         layout={MinimalLayout}
         path="/"
+      />
+      <RouteWithLayout
+        component={Register}
+        exact
+        layout={MinimalLayout}
+        path="/register"
+      />
+      <RouteWithLayout
+        component={Login}
+        exact
+        layout={MinimalLayout}
+        path="/login"
       />
       <RouteWithLayout
         component={NotFoundView}
